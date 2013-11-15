5847951860f9a289d56b18c7ca64fb36d668dd56
vientollc <rjohn@vientollc.com>
2013-11-15 18:25:01 +0100

    initial development

---
+++ www/js/stockroom.js
@@ -0,0 +1,7 @@
+$(function() {
+            Parse.initialize("Eto340s8ayyRdulcBtMJN5sPpfLr63YX7t6xxzAG", "unEzZIYE4MxVPqd0SFR7XOAGqmeo32ubwWQH46Nb");
+        });
+        
+        $('#loginBtn').on('click', function() {
+            $('#loginResp').text('Please wait, validating ...');
+        });
